1cd4717b3d1a6e049822278627f9815cf02537f8
krm <73038148+3xg3lin@users.noreply.github.com>
2023-09-24 00:58:18 +0300

    Update caesar_decipher.py

--- Rot cipher/caesar_decipher.py
+++ Rot cipher/caesar_decipher.py
@@ -2,21 +2,26 @@ import optparse
 
 def rot_func():
     instance = optparse.OptionParser()
-    instance.add_option('-t','--type',dest='type',default='13',help='which rot algorithm you want')
+    instance.add_option('-t','--type',dest='type',default = '13', help='which rot algorithm you want')
     return instance.parse_args()
 
-def decode_message(ctype,encrypted_message):
-    i = ''
+def encode_message(ctype,encrypt_message):
     alphabet = 'zZyYxXwWvVuUtTsSrRqQpPoOnNmMlLkKjJiIhHgGfFeEdDcCbBaA'
-    message=''
-    for i in encrypted_message:
-        index = 0
-        while i != alphabet[index]:
-            index += 1
-        message += alphabet[(index + ctype * 2)%52]
-    return message
+    i=''
+    string_message=''
+    for i in encrypt_message:
+        if i.isalpha():
+            counter = 0
+            for y in alphabet:
+                if i == y:
+                    string_message += alphabet[((counter + ctype * 2))%52]
+                    break
+                else:
+                    counter+=1
+        else:
+            string_message += i
+    return string_message
 
 (user_input,arguments) = rot_func()
-cipher_message = input('type a encrypted message: ')
-print(decode_message(int(user_input.type),cipher_message))
-
+encrypt_message=input('type a message: ')
+print(encode_message(int(user_input.type),encrypt_message))
