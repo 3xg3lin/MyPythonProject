6aa2091604ff7d6ea9bdfd038dddea0fa713a0e3
krm <73038148+3xg3lin@users.noreply.github.com>
2023-09-24 00:53:05 +0300

    Update caesar_cipher.py

--- Rot cipher/caesar_cipher.py
+++ Rot cipher/caesar_cipher.py
@@ -10,12 +10,18 @@ def encode_message(ctype,message):
     i=''
     string_message=''
     for i in message:
-        index = 0
-        while i != alphabet[index]:
-            index += 1
-        string_message += alphabet[(index + ctype * 2)%52]
+        if i.isalpha():
+            counter = 0
+            for y in alphabet:
+                if i == y:
+                    string_message += alphabet[((counter + ctype * 2))%52]
+                    break
+                else:
+                    counter+=1
+        else:
+            string_message += i
     return string_message
 
 (user_input,arguments) = rot_func()
 message=input('type a message: ')
-print(encode_message(int(user_input.type),message))
+print(encode_message(int(user_input.type),messag
